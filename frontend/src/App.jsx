aa686b134c9b85dd3a62f63b2d8b1742bae87e2a
Your Name <your.email@example.com>
2025-06-19 15:20:47 +0530

    remove backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
-import Navbar from './components/Navbar';
+
 import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
